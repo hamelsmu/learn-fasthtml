3ec6ef8366f6931adb131fd854d4938bc04dedea
Hamel Husain <hamel.husain@gmail.com>
2024-09-29 15:38:50 -0700

    save

--- 01_htmx_swap_fade/main.py
+++ 01_htmx_swap_fade/main.py
@@ -1,30 +1,27 @@
 from fasthtml.common import *
 
-app, rt = fast_app()
+# Define custom CSS for fade-out animation
+css = Style("""
+.fade-me-out.htmx-swapping {
+  opacity: 0;
+  transition: opacity 1s ease-out;
+}
+""")
+
+# Create a FastHTML app with custom CSS header
+app, rt = fast_app(hdrs=(css,))
 
 @rt("/")
 def get():
-    return Div(
-        "Load Data",
-        hx_get="/data",
-        hx_swap="outerHTML swap:300ms",
-        cls="fade-out"
-    )
+    # Return a Div containing a paragraph with HTMX attributes
+    return Div(P("Click Me to Change",
+        hx_get="/data",  # HTMX will make a GET request to /data when clicked
+        hx_swap="outerHTML swap:1s",  # Replace the entire element, with a 1s swap duration
+        cls="fade-me-out"  # Apply the fade-out class for animation
+    ))
 
 @rt("/data")
-def data():
-    return "New Data"
+def data(): return "New Data"  # Simple endpoint returning new content
 
-@app.route("/static/css/main.css")
-def css():
-    return """
-        .fade-out {
-            opacity: 1;
-            transition: opacity 300ms ease-out;
-        }
-        .fade-out.htmx-swapping {
-            opacity: 0;
-        }
-    """
-
+# Start the FastHTML server
 serve()
